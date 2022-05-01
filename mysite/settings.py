298d4805a50541abcb3d2efc1f85beee65e3920a
myfrank4everreal <myfrank4everreal@gmail.com>
2022-05-01 09:53:05 +0400

    sss

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,6 +134,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'webapp/static')
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
